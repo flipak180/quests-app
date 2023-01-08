7da1545547d4282330e9e185f93dbb3aceafe8bc
flipak180@mail.ru <flipak180@mail.ru>
2023-01-08 23:03:55 +0300

    map

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,21 +1,23 @@
 import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React from 'react';
+import {YMaps, Map, Placemark, GeolocationControl} from '@pbe/react-yandex-maps';
 
 const ProfilePage: React.FC = () => {
+    const defaultState = {
+        center: [59.938955, 30.315644],
+        zoom: 13,
+        controls: []
+    };
+
     return (
         <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Профиль</IonTitle>
-                </IonToolbar>
-            </IonHeader>
             <IonContent fullscreen>
-                <IonHeader collapse="condense">
-                    <IonToolbar>
-                        <IonTitle size="large">Профиль</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-                <ExploreContainer/>
+                <YMaps>
+                    <Map defaultState={defaultState} width="100vw" height="100vh">
+                        <Placemark geometry={[55.684758, 37.738521]} />
+                        <GeolocationControl options={{ float: "left", position: { bottom: 70, right: 20 } }} />
+                    </Map>
+                </YMaps>
             </IonContent>
         </IonPage>
     );
